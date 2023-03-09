5ee80608eb0662e898d97c06d5d881cb41417f1c
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-03-08 17:49:59 -0700

    lib/round.rb

--- lib/round.rb
+++ lib/round.rb
@@ -1,11 +1,18 @@
 class Round
-  attr_reader :deck
+  attr_reader :deck,
+              :turns
   
   def initialize(deck)
     @deck = deck
+    @turns = []
   end  
 
   def current_card
     deck.cards[0]
+  end
+
+  def take_turn(guess)
+    turns << Turn.new(guess, current_card)
+    turns[0]
   end
 end
